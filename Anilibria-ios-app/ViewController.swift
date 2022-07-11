bddee109dd920f4370c51ab8f5c0926319cc6182
MNasybullin <mansurnasybullin@gmail.com>
2022-07-11 17:48:36 +0300

    init project

---
+++ Anilibria-ios-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anilibria-ios-app
+//
+//  Created by Mansur Nasybullin on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
