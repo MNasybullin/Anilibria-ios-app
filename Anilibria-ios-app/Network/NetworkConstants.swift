f78ac5bbe0d0fc371aa67ab6afdcec4351769064
MNasybullin <mansurnasybullin@gmail.com>
2022-07-19 17:34:15 +0300

    added network templates

---
+++ Anilibria-ios-app/Network/NetworkConstants.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkConstants.swift
+//  Anilibria-ios-app
+//
+//  Created by Mansur Nasybullin on 19.07.2022.
+//
+
+import Foundation
+
+enum NetworkConstants {
+    
+}
---
+++ Anilibria-ios-app/Network/NetworkService.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkService.swift
+//  Anilibria-ios-app
+//
+//  Created by Mansur Nasybullin on 19.07.2022.
+//
+
+import Foundation
+
+protocol NetworkServiceProtocol {
+    
+}
+
+class NetworkService {
+    
+}
