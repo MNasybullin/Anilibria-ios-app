1f8d527c51e7f91c304c573af23403b2a8b0df4b
MNasybullin <mansurnasybullin@gmail.com>
2023-01-30 23:37:27 +0300

    Alert testing

--- Anilibria-ios-app/Common/Alert.swift
+++ Anilibria-ios-app/Common/Alert.swift
@@ -10,11 +10,11 @@ import UIKit
 
 struct Alert {
     static func showErrorAlert(on viewController: UIViewController, with title: String, message: String, completion: (() -> Void)? = nil) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: Strings.AlertController.Title.ok, style: .default)
+        alertController.addAction(alertAction)
+        
         DispatchQueue.main.async {
-            // Все в main потоке из-за: Если два потока вызовут эту функцию, то приложение крашится (Zombie objects или double free) в UIAlertController
-            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            let alertAction = UIAlertAction(title: Strings.AlertController.Title.ok, style: .default)
-            alertController.addAction(alertAction)
             viewController.present(alertController, animated: true, completion: completion)
         }
     }
