9bee1d140c41fc91b53dae3c82d360467397e2e1
MNasybullin <mansurnasybullin@gmail.com>
2023-02-02 13:29:18 +0300

    basis Search module

--- Anilibria-ios-app/Modules/Search/SearchViewController.swift
+++ Anilibria-ios-app/Modules/Search/SearchViewController.swift
@@ -14,10 +14,51 @@ protocol SearchViewProtocol: AnyObject {
 
 final class SearchViewController: UIViewController, SearchViewProtocol {
     var presenter: SearchPresenterProtocol!
+    var searchController: UISearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        configureSearchController()
+        configureNavigationItem()
     }
+    
+    private func configureSearchController() {
+        searchController = UISearchController(searchResultsController: ScheduleRouter.start(withNavigationController: UINavigationController()).entry)
+        searchController.delegate = self
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.autocapitalizationType = .none
+        searchController.definesPresentationContext = true
+    }
+    
+    private func configureNavigationItem() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        // TODO
+    }
+}
+
+// MARK: - UISearchResultsUpdating
+
+extension SearchViewController: UISearchResultsUpdating {
+    
+}
+
+// MARK: - UISearchBarDelegate
+
+extension SearchViewController: UISearchBarDelegate {
+    
+}
+
+// MARK: - UISearchControllerDelegate
+
+extension SearchViewController: UISearchControllerDelegate {
+    
 }
 
 #if DEBUG
