fdadc278470f551f6c77d06cb13b43d01c97ab8f
MNasybullin <mansurnasybullin@gmail.com>
2023-02-07 19:04:33 +0300

    created network monitor common class && connected to appDelegate

--- Anilibria-ios-app/AppDelegate.swift
+++ Anilibria-ios-app/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        NetworkMonitor.shared.startMonitoring()
         FirebaseApp.configure()
         Analytics.logEvent(AnalyticsEventAppOpen, parameters: nil)
         return true
