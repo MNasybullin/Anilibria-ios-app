7e2875d0adcdc5532c240a522165e2355292fc63
MNasybullin <mansurnasybullin@gmail.com>
2023-09-03 20:34:20 +0300

    SceneDelegate refactoring

--- Anilibria-ios-app/SceneDelegate.swift
+++ Anilibria-ios-app/SceneDelegate.swift
@@ -14,11 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
                 
-        let window = UIWindow(windowScene: windowScene)
-        let rootViewController = RootViewController.shared
-        window.rootViewController = rootViewController
-        self.window = window
-        window.makeKeyAndVisible()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = RootViewController.shared
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
