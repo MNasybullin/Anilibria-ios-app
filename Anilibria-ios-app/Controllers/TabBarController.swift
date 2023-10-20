f4cdb44ed6b90e399e8e733410324561b2b8abef
MNasybullin <mansurnasybullin@gmail.com>
2023-10-20 20:31:11 +0300

    fixed delegate in tabBarController

--- Anilibria-ios-app/Controllers/TabBarController.swift
+++ Anilibria-ios-app/Controllers/TabBarController.swift
@@ -17,6 +17,7 @@ final class TabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        delegate = self
         
         setupTabBar()
     }
