2f498edf4142ca2a0d4ed71d04a831e87d9738dc
MNasybullin <mansurnasybullin@gmail.com>
2024-03-01 16:39:12 +0300

    reconfigure Alert

--- Anilibria-ios-app/Common/Alert.swift
+++ Anilibria-ios-app/Common/Alert.swift
@@ -9,12 +9,14 @@ import Foundation
 import UIKit
 
 struct Alert {
-    static func showErrorAlert(on viewController: UIViewController, with title: String, message: String, completion: (() -> Void)? = nil) {
+    static func showErrorAlert(on viewController: UIViewController, title: String, message: String, buttonActionCompletion: (() -> Void)? = nil) {
         DispatchQueue.main.async {
             let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            let alertAction = UIAlertAction(title: Strings.AlertController.Title.ok, style: .default)
+            let alertAction = UIAlertAction(title: Strings.AlertController.Title.ok, style: .default) { _ in
+                buttonActionCompletion?()
+            }
             alertController.addAction(alertAction)
-            viewController.present(alertController, animated: true, completion: completion)
+            viewController.present(alertController, animated: true)
         }
     }
 }
